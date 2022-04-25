5fa0dda6241631d244a942b25fbf745c3f6a2bb7
Animesshh <104338471+Animesshh@users.noreply.github.com>
2022-04-25 09:31:53 +0530

    Add files via upload

---
+++ Student_details.py
@@ -0,0 +1,3 @@
+# program to enter a student's details into a list
+lst = [21102129, 'Animesh Negi', 'M', 'Civil Engineering', 7.0]
+print("The student's SID, Name, Gender, Branch and CGPA are as follows",lst)
